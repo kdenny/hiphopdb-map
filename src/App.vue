83de7d270abb7d5f47d8dca49da0f6798c9b16e3
kdenny <kdenny@deloitte.com>
2017-12-17 01:44:33 -0500

    Added bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,52 @@
 <template>
   <div>
+    <b-navbar toggleable="md" type="dark" variant="info">
+
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+      <b-collapse is-nav id="nav_collapse">
+
+        <b-navbar-nav>
+          <b-nav-item href="#">Link</b-nav-item>
+          <b-nav-item href="#" disabled>Disabled</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+
+          <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          </b-nav-form>
+
+          <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">ES</b-dropdown-item>
+            <b-dropdown-item href="#">RU</b-dropdown-item>
+            <b-dropdown-item href="#">FA</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown right>
+            <!-- Using button-content slot -->
+            <template slot="button-content">
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Signout</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+
+      </b-collapse>
+    </b-navbar>
+
     <ul id="side">
+      <li>
+        <b-btn @click="showAlert" variant="info" class="m-1">
+          Show alert
+        </b-btn>
+      </li>
       <li>
         <a href="/" @click="currentView='simple'">Simple map</a>
       </li>
@@ -87,6 +133,12 @@ export default {
     return {
       currentView: 'simple'
     }
+  },
+  methods: {
+    showAlert() {
+      alert('this is ' + 'sparta');
+    }
+
   }
 }
 </script>
@@ -99,6 +151,8 @@ export default {
   background-image: url(../node_modules/leaflet/dist/images/marker-shadow.png);
 }
 @import "../node_modules/leaflet/dist/leaflet.css";
+@import "../node_modules/bootstrap/dist/css/bootstrap.css";
+@import '../node_modules/bootstrap-vue/dist/bootstrap-vue.css';
 
 body {
   margin: 0px;
@@ -113,7 +167,7 @@ body {
 #full_div {
   position: absolute;
   overflow-x: auto;
-  top: 0;
+  top: 56px;
   right: 0;
   left: 208px;
   bottom: 0;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App'
+import BootstrapVue from 'bootstrap-vue'
 import router from './router'
 
 L.Icon.Default.imagePath = "/images/";
+
+Vue.use(BootstrapVue);
 
 new Vue({
   el: '#main',
